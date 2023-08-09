ff35aca4ec1066cd4327d0248ec4adc0115dc73b
x7upLime <andrew@andrewdomain.com>
2023-08-09 21:55:45 +0300

    Adds better optimized solution for twoSum

--- leetcode/twosum.go
+++ leetcode/twosum.go
@@ -1,17 +1,31 @@
 package main
 
 func TwoSum(nums []int, target int) []int {
-	for i, n := range nums {
-		for j, nn := range nums {
-			if i == j {
-				continue
-			}
-
-			if n+nn == target {
-				return []int{i, j}
-			}
+	nnn := make(map[int]int) // a nums/index map
+	for i := 0; i < len(nums); i++ {
+		if nn, ok := nnn[target-nums[i]]; ok {
+			return []int{nn, i}
 		}
+		nnn[nums[i]] = i
 	}
 
 	return nil
+	// Best solution in speed.. consumes more memory than the following tho O.o
+	// Alternative:
+	/*
+	   for i, n := range nums {
+	           for j, nn := range nums {
+	                   if i == j {
+	                           continue
+	                   }
+
+	                   if n+nn == target {
+	                           return []int{i, j}
+	                   }
+	           }
+	   }
+
+	   return nil
+
+	*/
 }
