848a75f61d7b4e115fe75926e8224eafb3a75a79
x7upLime <andrew@andrewdomain.com>
2023-08-21 00:19:13 +0300

    Polishes ht_test.c

--- dsalgo/ht/ht_test.c
+++ dsalgo/ht/ht_test.c
@@ -6,43 +6,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-const char*
-ht_set_entry(ht_entry *entries, size_t capacity,
-	     const char *key, void *value, size_t *plength);
+const char* ht_set_entry(ht_entry *entries, size_t capacity,
+			 const char *key, void *value, size_t *plength);
 
 typedef struct{
   const char *name;
   void (*run)(void);
-} test;
+} TestCase;
 
-void
-foo(void) {
-  return;
-}
+#define INITCAP 15
 
 void
 TestHT_Set_EntryNULLPlength(void) {
   // random key/value
   const char *key = strdup("Arandomkey");
   int value = 11;
+  int cap = 10;
+  ht_entry *hte;
 
-  ht_entry *entries;
-  size_t htcap;
-
-  size_t *plength = NULL;
+  hte = (ht_entry*) calloc(cap, sizeof(ht_entry));
   
-  ht_set_entry(entries, htcap, key, (void*)&value, plength);
+  ht_set_entry(hte, cap, key, &value, NULL);
 }
 
 int
 main(void) {
-  test tests[] = {
+  TestCase tests[] = {
     {"Testing ht_set_entry with NULL plength", TestHT_Set_EntryNULLPlength},
-    {"asdf", foo}
   };
 
-  for (int i = 0; i < sizeof(tests)/sizeof(test); i++) {
+  for (size_t i = 0; i < sizeof(tests)/sizeof(tests[0]); i++) {
     printf("%s:", tests[i].name);
     tests[i].run();
     printf("\tPASS\n");
